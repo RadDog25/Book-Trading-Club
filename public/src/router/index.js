d9738315fecb124ec91bf2a6d3afcc21264360f8
Chris Geelhoed <cgeelhoed@wearebraid.com>
2018-02-12 14:51:29 -0800

    Add routes for pages

--- public/src/router/index.js
+++ public/src/router/index.js
@@ -1,6 +1,11 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Home from '@/components/Home'
+import Register from '@/components/Register'
+import Login from '@/components/Login'
+import Books from '@/components/Books'
+import Dashboard from '@/components/Dashboard'
+import Profile from '@/components/Profile'
 
 Vue.use(Router)
 
@@ -8,8 +13,33 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Home',
+      component: Home
+    },
+    {
+      path: '/register',
+      name: 'Register',
+      component: Register
+    },
+    {
+      path: '/login',
+      name: 'Login',
+      component: Login
+    },
+    {
+      path: '/books',
+      name: 'Books',
+      component: Books
+    },
+    {
+      path: '/dashboard',
+      name: 'Dashboard',
+      component: Dashboard
+    },
+    {
+      path: '/profile',
+      name: 'Profile',
+      component: Profile
     }
   ]
 })
