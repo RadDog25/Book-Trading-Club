bcde72dea26ef0e19ab931b2b0b93fc5e32b40b3
Chris Geelhoed <cgeelhoed@wearebraid.com>
2018-02-17 21:12:48 -0800

    Install mongoose

--- app.js
+++ app.js
@@ -3,10 +3,25 @@ var path = require('path');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var bluebird = require('bluebird');
 
 var users = require('./routes/users');
 
 var app = express();
+
+// mongodb://<dbuser>:<dbpassword>@ds239648.mlab.com:39648/book-trading-club-dev
+
+var env = process.env.NODE_ENV || 'development';
+var config = require('./config')[env];
+
+var dbUrl = `mongodb://${config.username}:${config.password}@${config.url}:${config.port}/${config.db}`;
+mongoose.Promise = require('bluebird');
+mongoose.connect(dbUrl);
+
+// var db = mongoose.connection;
+// db.on('error', console.error.bind(console, 'connection error:'));
+// db.once('open', console.log.bind(console, 'connection success!'));
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
