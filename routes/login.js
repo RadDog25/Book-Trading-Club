38603506be5a90ec8d15a4d6508441cbde71e264
Chris Geelhoed <cgeelhoed@wearebraid.com>
2018-02-25 16:01:03 -0800

    Set api for login route

--- routes/login.js
+++ routes/login.js
@@ -2,38 +2,58 @@ var express = require('express');
 var router = express.Router();
 var jwt = require('jsonwebtoken');
 var config = require('../config');
+var FormErrors = require('../helpers/FormErrors.js');
 
 // Models
 var User = require('../models/User.js');
 
 router.post('/', function(req, res) {
-    User.findOne({
-        username: req.body.username
-    }, function(err, user) {
-        if (err) throw err;
-
-        if (!user) {
-            res
-                .status(401)
-                .json({ success: false, message: 'Authentication failed. User not found.' });
-        } else {
-            // check if password matches
-            user.comparePassword(req.body.password, function (err, isMatch) {
-                if (isMatch && !err) {
-                    // if user is found and password is right create a token
-                    var token = jwt.sign(user.toJSON(), config.secret);
-                    // return the information including token as JSON
-                    res
-                        .status(200)
-                        .json({ success: true, token: 'JWT ' + token });
-                } else {
-                    res
-                        .status(401)
-                        .json({ success: false, message: 'Authentication failed. Wrong password.' });
-                }
-            });
-        }
+    var formErrors = new FormErrors(req, {
+        requiredFields: [ 'username', 'password' ]
     });
+
+    if( formErrors.any() ) {
+        res
+            .status(400)
+            .send( formErrors.get() );
+    } else {
+        User.findOne({
+            username: req.body.username
+        }, function(err, user) {
+            if (err) throw err;
+
+            if (!user) {
+                formErrors.set('username', 'Username does not exist');
+                res
+                    .status(401)
+                    .send( formErrors.get() );
+            } else {
+                // check if password matches
+                user.comparePassword(req.body.password, function (err, isMatch) {
+                    if (isMatch && !err) {
+                        // if user is found and password is right create a token
+                        var token = jwt.sign(user.toJSON(), config.secret);
+
+                        console.log(res.cookie)
+                        
+                        res
+                            .status(200)
+                            .cookie('token', token, {
+                                maxAge: 1000 * 60 * 60 * 24,
+                                httpOnly: true
+                            })
+                            .send(token);
+                            //.send('The token has been added as a cookie');
+                    } else {
+                        formErrors.set('password', 'Wrong password');
+                        res
+                            .status(401)
+                            .send( formErrors.get() );
+                    }
+                });
+            }
+        });
+    }
 });
 
 module.exports = router;
