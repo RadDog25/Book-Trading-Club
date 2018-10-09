94d52b344d24d1f46c2af9afb03cbf2ddc713d86
Chris Geelhoed <cgeelhoed@wearebraid.com>
2018-10-08 18:50:17 -0700

    Clean up trade route using async await

--- routes/trade.js
+++ routes/trade.js
@@ -11,46 +11,51 @@ var options = {
     setDefaultsOnInsert: true
 };
 
-router.post('/', passport.authenticate('jwt', { session: false }), function(req, res) {
-    var tradeRequestId = req.body.id;
-    var bookInstanceForOwner = req.body.bookInstanceForOwner;
-    var bookInstanceForRequester = req.body.bookInstanceForRequester;
-    var action = req.body.action;
-    var user = req.user;
+router.post('/', passport.authenticate('jwt', { session: false }), async function(req, res) {
+    try {
+        var tradeRequestId = req.body.id;
+        var bookInstanceForOwner = req.body.bookInstanceForOwner;
+        var action = req.body.action;
+        var user = req.user;
 
-    // first check that user is part of this trade, that they did not make the last action, and that the trade is not cancelled
+        var tradeRequest = await TradeRequest.findOne({ _id: tradeRequestId })
+            .or([
+                { $and: [{ owner: user._id }, { lastActionWasRequester: true }] },
+                { $and: [{ requester: user._id }, { lastActionWasRequester: false }] }
+            ])
+            .where('status').in(['initiated', 'proposed'])
+            .exec();
 
-    TradeRequest.findOne({ _id: tradeRequestId })
-        .or([
-            { $and: [{ owner: user._id }, { lastActionWasRequester: true }] },
-            { $and: [{ requester: user._id }, { lastActionWasRequester: false }] }
-        ])
-        .where('status').in(['initiated', 'proposed'])
-        .exec(async function(err, tradeRequest) {
-            tradeRequest.lastActionWasRequester = !tradeRequest.lastActionWasRequester;
+        if (action == 'accept') {
 
-            if (action == 'accept' && tradeRequest.bookInstanceForOwner && tradeRequest.bookInstanceForRequester) {
+            await BookInstance.findOneAndUpdate({ _id: tradeRequest.bookInstanceForOwner }, {
+                user: tradeRequest.owner
+            }).exec();
 
+            await BookInstance.findOneAndUpdate({ _id: tradeRequest.bookInstanceForRequester }, {
+                user: tradeRequest.requester
+            }).exec();
 
+            tradeRequest.status = 'accepted';
 
-                tradeRequest.status = 'accepted';
+        } else if (action == 'decline') {
+            tradeRequest.status = 'declined';
+        } else if (action == 'propose' && bookInstanceForOwner) {
+            tradeRequest.status = 'proposed';
+            tradeRequest.bookInstanceForOwner = bookInstanceForOwner;
+        } else {
+            res.status(401).send();
+        }
 
-            } else if (action == 'decline') {
-                tradeRequest.status = 'declined';
-            } else if (action == 'propose' && bookInstanceForOwner) {
-                tradeRequest.status = 'proposed';
-                tradeRequest.bookInstanceForOwner = bookInstanceForOwner;
-            } else {
-                res.status(401).send();
-            }
+        tradeRequest.lastActionWasRequester = !tradeRequest.lastActionWasRequester;
+        await tradeRequest.save();
+        var userData = await getUserData(user);
+        res.send(userData);
 
-            tradeRequest.save(err => {
-                getUserData(user)
-                    .then(userData => {
-                        res.send(userData);
-                    });
-            });
-        });
+    } catch (error) {
+        console.log(error);
+        res.status(400).send();
+    }
 });
 
 module.exports = router;
