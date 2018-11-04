4a3f5d45299d5cfe2aa544ff0bbb050905812115
Chris Geelhoed <cgeelhoed@wearebraid.com>
2018-11-03 23:08:10 -0700

    Move user details changes to user route

--- routes/users.js
+++ routes/users.js
@@ -2,12 +2,12 @@ var express = require('express');
 var router = express.Router();
 var passport = require('passport');
 var User = require('../models/User.js');
+var FormErrors = require('../helpers/FormErrors.js');
 
 router.get('/:id', passport.authenticate('jwt', { session: false }), async function(req, res) {
     try {
         var id = req.params.id;
         var user;
-        console.log(id);
 
         if (id === 'me') {
             user = req.user;
@@ -16,8 +16,88 @@ router.get('/:id', passport.authenticate('jwt', { session: false }), async funct
         }
 
         var userData = await user.getData();
-        console.log(userData);
         res.send(userData);
+
+    } catch (error) {
+        console.log(error);
+        res.status(400).send();
+    }
+});
+
+router.put('/me/avatar', passport.authenticate('jwt', { session: false }), async function(req, res) {
+    try {
+
+        var user = req.user;
+        user.avatar = req.body.avatar;
+        await user.save();
+        var userData = await user.getData();
+        res.send(userData);
+
+    } catch (error) {
+        console.log(error);
+        res.status(400).send();
+    }
+});
+
+router.put('/me/location', passport.authenticate('jwt', { session: false }), async function(req, res) {
+    try {
+        var formErrors = new FormErrors(req, {
+            requiredFields: [ 'location' ]
+        });
+    
+        if (formErrors.any()) {
+            res.status(400).send(formErrors.get());
+            return;
+        }
+
+        var user = req.user;
+        user.location = req.body.location;
+        await user.save();
+        var userData = await user.getData();
+        res.send(userData);
+            
+    } catch (error) {
+        console.log(error);
+        res.status(400).send();
+    }
+});
+
+router.put('/me/password', passport.authenticate('jwt', { session: false }), async function(req, res) {
+    try {
+        var formErrors = new FormErrors(req, {
+            requiredFields: [
+                {
+                    key: 'oldPassword',
+                    label: 'old password'
+                },
+                {
+                    key: 'newPassword',
+                    label: 'new password'
+                }
+            ]
+        });
+    
+        if (formErrors.any()) {
+            res.status(400).send(formErrors.get());
+            return;
+        }
+
+        // check if password matches
+        var user = req.user;
+        user.comparePassword(req.body.oldPassword, async function(err, isMatch) {
+
+            if (isMatch && !err) {
+
+                user.password = req.body.newPassword;
+                await user.save();
+                res.status(200).send();
+
+            } else {
+                formErrors.set('oldPassword', 'Wrong password');
+                res.status(401).send(formErrors.get());
+            }
+        });
+            
     } catch (error) {
         console.log(error);
         res.status(400).send();
