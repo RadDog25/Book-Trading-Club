2f6d32e60b2e9650d8f2b2f90ca75862123e8337
Chris Geelhoed <cgeelhoed@wearebraid.com>
2018-11-08 18:33:23 -0800

    Add model whitelists for front end data

---
+++ whitelists/book.js
@@ -0,0 +1,13 @@
+module.exports = [
+    'authors',
+    'categories',
+    'description',
+    'id',
+    'link',
+    'publishedDate',
+    'publisher',
+    'thumbnail',
+    'title',
+    'ratingsCount',
+    'averageRating'
+];
