04c730f1a44757fe89070a0395353f1c859a21e6
Chris Geelhoed <cgeelhoed@wearebraid.com>
2018-11-08 17:59:06 -0800

    Remove unused helper functions

--- helpers/getSearchedBooks.js
+++
@@ -1,9 +0,0 @@
-function getSearchedBooks(books, searchText) {
-    return books.filter(book => {
-        var bookText = JSON.stringify(Object.values(book));
-
-        return bookText.includes(searchText);
-    });
-}
-
-module.exports = getSearchedBooks;
