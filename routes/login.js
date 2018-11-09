40a6504268331f347b6be4fadb1060d6ff7d82d8
Chris Geelhoed <cgeelhoed@wearebraid.com>
2018-11-08 17:52:08 -0800

    Refactor login and register routes

--- routes/login.js
+++ routes/login.js
@@ -3,57 +3,50 @@ var router = express.Router();
 var jwt = require('jsonwebtoken');
 var config = require('../config');
 var FormErrors = require('../helpers/FormErrors.js');
-var getUserData = require('../helpers/getUserData.js');
-
-// Models
 var User = require('../models/User.js');
 
-router.post('/', function(req, res) {
-    var formErrors = new FormErrors(req, {
-        requiredFields: [ 'username', 'password' ]
-    });
-
-    if( formErrors.any() ) {
-        res
-            .status(400)
-            .send( formErrors.get() );
-    } else {
-        User.findOne({
+router.post('/', async function(req, res) {
+    try {
+        var formErrors = new FormErrors(req, {
+            requiredFields: [ 'username', 'password' ]
+        });
+    
+        if (formErrors.any()) {
+            res.status(400).send(formErrors.get());
+            return;
+        }
+    
+        var user = await User.findOne({
             username: req.body.username
-        }, function(err, user) {
-            if (err) throw err;
-
-            if (!user) {
-                formErrors.set('username', 'Username does not exist');
-                res
-                    .status(401)
-                    .send( formErrors.get() );
+        });
+    
+        if (!user) {
+            formErrors.set('username', 'Username does not exist');
+            res.status(401).send(formErrors.get());
+            return;
+        }
+    
+        // check if password matches
+        user.comparePassword(req.body.password, async function (err, isMatch) {
+            if (isMatch && !err) {
+                // if user is found and password is right create a token
+                var token = jwt.sign(user.toJSON(), config.secret);
+                var userData = await user.getData();
+                res.status(200)
+                    .cookie('token', token, {
+                        httpOnly: false
+                    })
+                    .send(userData);
+                    
             } else {
-                // check if password matches
-                user.comparePassword(req.body.password, function (err, isMatch) {
-                    if (isMatch && !err) {
-                        // if user is found and password is right create a token
-                        var token = jwt.sign(user.toJSON(), config.secret);
-
-                        getUserData(user)
-                            .then(userData => {
-                                res.status(200)
-                                    .cookie('token', token, {
-                                        httpOnly: false
-                                    })
-                                    .send(userData);
-                            })
-                            .catch(err => console.log(err));
-
-                    } else {
-                        formErrors.set('password', 'Wrong password');
-                        res
-                            .status(401)
-                            .send( formErrors.get() );
-                    }
-                });
+                formErrors.set('password', 'Wrong password');
+                res.status(401).send(formErrors.get());
             }
         });
+
+    } catch (error) {
+        console.log(error);
+        res.send(400);
     }
 });
 
--- routes/register.js
+++ routes/register.js
@@ -1,39 +1,33 @@
 var express = require('express');
 var FormErrors = require('../helpers/FormErrors.js');
 var router = express.Router();
-
-// Models
 var User = require('../models/User.js');
 
 router.post('/', function(req, res) {
 
     var formErrors = new FormErrors(req, {
-        requiredFields: [ 'username', 'password' ]
+        requiredFields: ['username', 'password']
     });
 
-    if ( formErrors.any() ) {
-        res
-            .status(400)
-            .send( formErrors.get() );
-    } else {
-        var newUser = new User({
-            username: req.body.username,
-            password: req.body.password
-        });
-
-        newUser.save(function(err) {
-            if (err) {
-                formErrors.set('username', 'Username already exists');
-                res
-                    .status(400)
-                    .send( formErrors.get() );
-            } else {
-                res
-                    .status(201)
-                    .send('Successful created new user');
-            }
-        });
+    if (formErrors.any()) {
+        res.status(400).send(formErrors.get());
+        return;
     }
+
+    var newUser = new User({
+        username: req.body.username,
+        password: req.body.password
+    });
+
+    newUser.save(function(err) {
+        if (err) {
+            formErrors.set('username', 'Username already exists');
+            res.status(400).send(formErrors.get());
+        } else {
+            var message = 'Successful created new user';
+            res.status(201).send(message);
+        }
+    });
 });
 
 module.exports = router;
