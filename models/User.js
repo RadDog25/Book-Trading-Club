441e9864f4f2435727fdc522859d43952e0fe6c6
Chris Geelhoed <cgeelhoed@wearebraid.com>
2018-11-08 18:10:19 -0800

    Add methods to user model

--- models/User.js
+++ models/User.js
@@ -1,6 +1,11 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 var bcrypt = require('bcrypt-nodejs');
+var Book = require('../models/Book.js');
+var BookInstance = require('../models/BookInstance.js');
+var TradeRequest = require('../models/TradeRequest.js');
+var userPropertiesToSend = require('../whitelists/user.js');
+var bookPropertiesToSend = require('../whitelists/book.js');
 
 var UserSchema = new Schema({
     username: {
@@ -26,6 +31,64 @@ var UserSchema = new Schema({
     timestamps: true
 });
 
+UserSchema.methods.getProperties = function() {
+    var properties = {};
+    for(property of userPropertiesToSend) {
+        properties[property] = this[property];
+    }
+
+    return properties;
+}
+
+UserSchema.methods.getBookInstances = function() {
+    return BookInstance.find({ user: this._id })
+        .select('book')
+        .populate({ path: 'book', select: bookPropertiesToSend.join(' ') })
+        .exec();
+}
+
+UserSchema.methods.getTradeRequests = function() {
+    return TradeRequest.find()
+        .or([{ owner: this._id }, { requester: this._id }])
+        .populate([
+            { path: 'owner', select: userPropertiesToSend.join(' ') },
+            { path: 'requester', select: userPropertiesToSend.join(' ') },
+        ])
+        .deepPopulate(['bookInstanceForOwner.book', 'bookInstanceForRequester.book'])
+        .exec();
+}
+
+UserSchema.methods.getData = function() {
+    return Promise.all([this.getBookInstances(), this.getTradeRequests()])
+        .then(([bookInstances, tradeRequests] = data) => {
+            return {
+                ...this.getProperties(),
+                bookInstances,
+                tradeRequests
+            }
+        });
+}
+
+UserSchema.methods.addBook = function(book) {
+
+    var options = {
+        upsert: true,
+        new: true,
+        setDefaultsOnInsert: true
+    };
+
+    return Book.findOneAndUpdate({ id: book.id }, book, options)
+        .exec()
+        .then(newBook => {
+            var bookInstance = new BookInstance({
+                book: newBook._id,
+                user: this._id
+            });
+
+            return bookInstance.save();
+        });
+}
+
 UserSchema.pre('save', function (next) {
     var user = this;
     if (this.isModified('password') || this.isNew) {
@@ -33,6 +96,7 @@ UserSchema.pre('save', function (next) {
             if (err) {
                 return next(err);
             }
+            
             bcrypt.hash(user.password, salt, null, function (err, hash) {
                 if (err) {
                     return next(err);
